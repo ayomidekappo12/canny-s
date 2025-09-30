f9068973dd8d8e8220670dcbe404d44283173bcc
ayomidekappo12 <ayomidekappo12@gmail.com>
2025-09-30 08:27:21 -0500

    made a new change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { getSessionData } from "@/lib/session";
 import axios from "axios";
-import { normalizeError, AppError } from "@/lib/errors";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -29,36 +27,4 @@ interface SessionData {
   role?: string;
 }
 
-// Custom Error wrapper for normalized errors
-class NormalizedError extends Error {
-  code?: number | string;
-
-  constructor(appError: AppError) {
-    super(appError.message);
-    this.code = appError.code;
-    this.name = "NormalizedError";
-  }
-}
-
-export const fetcher = async <T = unknown>(url: string): Promise<T> => {
-  let session: SessionData;
-  try {
-    const raw = await getSessionData();
-    session =
-      typeof raw === "string"
-        ? (JSON.parse(raw) as SessionData)
-        : (raw as SessionData);
-  } catch {
-    session = {};
-  }
-
-  const token = session.aut;
-  const config = token ? { headers: { Authorization: `Bearer ${token}` } } : {};
-
-  try {
-    const res = await api.get<T>(url, config);
-    return res.data;
-  } catch (err) {
-    throw new NormalizedError(normalizeError(err));
-  }
-};
+// Custom Error wrapper for normalized error
