f139d55692e309099237396b4e21a6a361cc2510
ayomidekappo12 <ayomidekappo12@gmail.com>
2025-09-30 10:49:14 -0500

    made new changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,30 +1,9 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-});
-
-export const debugLog = (...args: unknown[]) => {
-  if (process.env.NODE_ENV === "development") {
-    console.log(...args);
-  }
-};
-
-export const debugError = (...args: unknown[]) => {
-  if (process.env.NODE_ENV === "development") {
-    console.error(...args);
-  }
-};
-
-interface SessionData {
-  aut?: string;
-  role?: string;
-}
 
 // Custom Error wrapper for normalized error
